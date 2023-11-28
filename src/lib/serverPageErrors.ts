2b120febb2c7c93a4700c1927cccb32dec8c248d
Alexander Kadyrov <gruz0.mail@gmail.com>
2023-11-28 21:15:05 +0400

    Activate Sentry

--- src/lib/serverPageErrors.ts
+++ src/lib/serverPageErrors.ts
@@ -1,3 +1,5 @@
+import { Prisma } from '@prisma/client'
+import * as Sentry from '@sentry/nextjs'
 import { ClientError, UseCaseValidationError } from '@/useCases/errors'
 
 export interface ServerPageErrors {
@@ -12,6 +14,17 @@ export const captureServerPageError = (e: unknown): ServerPageErrors => {
 
   if (e instanceof ClientError) {
     return { errorMessage: e.message }
+  }
+
+  // We want to capture either unknown or server errors
+  Sentry.captureException(e)
+
+  if (e instanceof Prisma.PrismaClientKnownRequestError) {
+    return { errorMessage: 'Database error' }
+  }
+
+  if (e instanceof Prisma.PrismaClientInitializationError) {
+    return { errorMessage: 'Database is not ready' }
   }
 
   return { errorMessage: `${e}` }
